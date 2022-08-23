5a616eae028630fbd2ea6fa4e6bf65676c2e548d
Tomas Chirino <tomasch2000@hotmail.com>
2022-08-23 19:05:16 -0300

    Creando el NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
+      <NavBar />
       <h1>Proyecto React - Coderhouse</h1>
     </div>
   );
